b42c5ea72a4c103fe76adcbbf6ade6b9d425eb77
Raheela Khan <raheelak20@gmail.com>
2016-12-04 15:34:56 -0800

    Send e-mail when change occurs

--- index.js
+++ index.js
@@ -2,17 +2,35 @@
 
 var Horseman = require('node-horseman');
 var phantomJS = new Horseman({
-  phantomPath: '/Users/raheelakhan/bin/phantomjs',
+  phantomPath: process.env.PHANTOMPATH,
   loadImages:true,
   injectJquery:true,
   webSecurity:true,
   ignoreSSQLErrors:true
 });
+var express = require('express'),
+    nodeMailer = require('nodemailer'),
+    app = express(),
+    SMTPTransport = nodeMailer.createTransport({
+      service:"gmail",
+      auth: {
+        user:process.env.USER,  
+        pass:process.env.PASSWORD
+      }
+    }),
+    mailOptions = {
+      from:process.env.User,
+      to: process.argv[3],
+      subject: 'woot change!',
+      text: 'hey you, somethings up at ' + process.argv[2]
+    };
 
 var appData = {
   targetIds: [1100,1000,900,906,1005,1006,1007,908,907,905,901,804,806,808,803,805,706,708,1008,1107,1105,1106,1108,1110]
 };
 
+app.listen(3000, function() {
+console.log("listening", process.env.User,process.env.Password);
 phantomJS
   .open(process.argv[2])
   .wait(10000) // let the test page load
@@ -38,13 +56,25 @@ phantomJS
     }
    return html; 
   }).then(function(result) {
+    var changes = [];
     //send out e-mail if missing
     for (var i = 0; i < appData.targetIds.length;i++) {
-      if (!result.indexOf(targetIds[i]) {
-         
+      if (!result.indexOf(appData.targetIds[i])) {
+        changes.push(result[i]); 
       }
     }
+    if (changes.length) {
+      mailOptions.text += "\n The following IDs changed :\n" + changes.join(", "); 
+      SMTPTransport.sendMail(mailOptions, function(err,res) {
+      if (err) {
+        console.log("oops", err);
+      } else {
+        console.log("Message sent " + res.message);
+      }
+    });
+    }  
   }).catch(function(err) {
     console.log("something isn't right here", err);
   })
-  .close();
+  .close()
+});
